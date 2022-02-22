b5efb1b2e18853222e359f87288db90303d9e97d
Mervin Worm <mervinworm@gmail.com>
2022-02-21 23:11:35 -0300

    Se agrega el ciclo for para la lista

--- Proyecto.py
+++ Proyecto.py
@@ -2,11 +2,12 @@ def run():
     my_list = [1, 2, 3, 4, 5]
 
     my_dict = {'uno': 1, 'dos': 2,'tres': 3}
-
-    print(my_list)
-
+    
     for name, value  in my_dict.items():
         print(name, " ", value)
+
+    for i in my_list:
+        print(i)
 
 if __name__ == '__main__':
     run()
